2ca3a77dffd89eaf160d268ab506596136f65fde
Arian1192 <Arian.collaso.rodriguez@gmail.com>
2023-04-10 19:12:00 +0200

    Add a video background on SignIn and SignUp

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -2,9 +2,19 @@ import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sing-in" />
+    <div className="relative">
+      <video
+        loop
+        autoPlay
+        muted
+        className="absolute inset-0 z-[-1] h-full w-full object-cover"
+        src="fondo-49311.mp4"
+      />
+      <div className="flex h-screen w-full items-center justify-center">
+        <SignUp path="/sign-up" routing="path" signInUrl="/sing-in" />;
+      </div>
     </div>
   );
 };
 export default SignUpPage;
+
